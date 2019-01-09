620707376b7d5ec7732762efea0cd02390cde2f5
yuxing555 <2219502781@qq.com>
2019-01-09 10:44:10 +0800

    add ve file

---
+++ VTKLearn/VTKModelLearn/vecontrol.cpp
@@ -0,0 +1,5 @@
+#include "vecontrol.h"
+
+VEControl::VEControl()
+{
+}
---
+++ VTKLearn/VTKModelLearn/vecontrol.h
@@ -0,0 +1,10 @@
+#ifndef VECONTROL_H
+#define VECONTROL_H
+
+class VEControl
+{
+public:
+	VEControl();
+};
+
+#endif // VECONTROL_H
